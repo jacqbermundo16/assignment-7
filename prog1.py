5fbf6aac91c2d3dde219914f1eb66b2951627c71
Jacqueline C Bermundo <92629695+jacqbermundo16@users.noreply.github.com>
2021-12-03 10:02:59 +0800

    ask for sentence input

---
+++ prog1.py
@@ -0,0 +1,22 @@
+#Create a program that ask for a sentence as input.
+#Display the number of words, vowels and consonants in the input 
+#ex.
+#input: Bahala kayo dyan
+#output:
+#words: 3
+#vowels: 6
+#consonants: 8
+
+
+#steps
+# ask for input
+def askInput():
+    _sentence = input("Write a sentence: ")
+    return _sentence
+
+# create a function that counts the number of words
+# create a function that counts the number of vowels
+# create a function that counts the number of consonants
+# display the results
+
+sentence = askInput() 
