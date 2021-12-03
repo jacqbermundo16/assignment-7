ae253dc27026e88980a2741847bab28f9ca53160
Jacqueline C Bermundo <92629695+jacqbermundo16@users.noreply.github.com>
2021-12-03 11:20:20 +0800

    counts the vowels within the sentence

--- prog1.py
+++ prog1.py
@@ -14,7 +14,7 @@ def askInput():
     _sentence = input("Write a sentence: ")
     return _sentence
 
-
+# create a function that counts the number of words
 def count(sentencef):
     countWord = 1
     for w in sentencef:
@@ -22,11 +22,20 @@ def count(sentencef):
             countWord = countWord + 1
     return countWord
 
-# create a function that counts the number of words
 # create a function that counts the number of vowels
+def count2(sentenceg):
+    countVowels = 0
+    char = 'aeiou'
+    for v in sentenceg:
+        for d in char:
+            if v == d:
+                countVowels = countVowels + 1
+    return countVowels
+    
 # create a function that counts the number of consonants
 # display the results
 
 sentence = askInput()
 words = count(sentence)
-print(words)
+vowels = count2(sentence)
+print(words, vowels)
